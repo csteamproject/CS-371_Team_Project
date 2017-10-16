38ef4c9c48535936e975dbd2a568994202f4ef6f
McGovern95 <mcgovernchristian@gmail.com>
2017-10-15 18:05:59 -0600

    updated, working on documetation

--- Scratch/Scratch/src/Map/TileMap.cs
+++ Scratch/Scratch/src/Map/TileMap.cs
@@ -41,63 +41,26 @@ namespace Scratch
 			this.squaresDown = squaresDown;
 			this.squaresAcross = squaresAcross;
 
-            // Create Sample Map Data
-            Rows[0].Columns[0].TileID = 6;
-            
-            Rows[0].Columns[3].TileID = 1;
-            Rows[0].Columns[4].TileID = 1;
-            Rows[0].Columns[5].TileID = 1;
-            Rows[0].Columns[6].TileID = 1;
-            Rows[0].Columns[7].TileID = 1;
+            /*
+                Tile IDs
+                1 = default
+                2 - 19 = slight color variations of default
 
-            Rows[1].Columns[3].TileID = 1;
-            Rows[1].Columns[3].TileID = 78;
-            Rows[1].Columns[4].TileID = 1;
-            Rows[1].Columns[5].TileID = 1;
-            Rows[1].Columns[6].TileID = 1;
-            Rows[1].Columns[7].TileID = 1;
-
-            Rows[2].Columns[2].TileID = 1;
-            Rows[2].Columns[3].TileID = 1;
-            Rows[2].Columns[4].TileID = 1;
-            Rows[2].Columns[5].TileID = 1;
-            Rows[2].Columns[6].TileID = 1;
-            Rows[2].Columns[7].TileID = 1;
-
-            Rows[3].Columns[2].TileID = 1;
-            Rows[3].Columns[3].TileID = 1;
-            Rows[3].Columns[4].TileID = 1;
-            Rows[3].Columns[5].TileID = 1;
-            Rows[3].Columns[6].TileID = 1;
-            Rows[3].Columns[7].TileID = 1;
-
-            Rows[4].Columns[2].TileID = 1;
-            Rows[4].Columns[3].TileID = 1;
-            Rows[4].Columns[4].TileID = 1;
-            Rows[4].Columns[5].TileID = 2;
-            Rows[4].Columns[6].TileID = 2;
-            Rows[4].Columns[7].TileID = 2;
-            Rows[4].Columns[7].TileID = 130;
+            */
 
 
-            Rows[5].Columns[2].TileID = 1;
-            Rows[5].Columns[3].TileID = 1;
-            Rows[5].Columns[4].TileID = 1;
-            Rows[5].Columns[5].TileID = 1;
-            Rows[5].Columns[6].TileID = 1;
-            Rows[5].Columns[7].TileID = 1;
+          
+            // Rows[3].Columns[2].TileID = 1;
+           
 
             Rows[16].Columns[4].AddHeightTile(54);
-
             Rows[17].Columns[3].AddHeightTile(54);
-
             Rows[15].Columns[3].AddHeightTile(54);
             Rows[16].Columns[3].AddHeightTile(53);
-
             Rows[15].Columns[4].AddHeightTile(54);
             Rows[15].Columns[4].AddHeightTile(54);
             Rows[15].Columns[4].AddHeightTile(51);
-
+      
             Rows[18].Columns[3].AddHeightTile(51);
             Rows[19].Columns[3].AddHeightTile(50);
             Rows[18].Columns[4].AddHeightTile(55);
@@ -113,23 +76,31 @@ namespace Scratch
             Rows[14].Columns[4].AddTopperTile(125);
             Rows[15].Columns[5].AddTopperTile(91);
             Rows[16].Columns[6].AddTopperTile(94);
-            Rows[3].Columns[5].AddBaseTile(30);
-            Rows[4].Columns[5].AddBaseTile(27);
-            Rows[5].Columns[5].AddBaseTile(28);
+       
 
-            Rows[3].Columns[6].AddBaseTile(25);
-            Rows[5].Columns[6].AddBaseTile(24);
+            Rows[36].Columns[24].AddHeightTile(54);
+            Rows[37].Columns[23].AddHeightTile(54);
+            Rows[33].Columns[23].AddHeightTile(54);
+            Rows[36].Columns[23].AddHeightTile(53);
+            Rows[35].Columns[24].AddHeightTile(54);
+            Rows[35].Columns[24].AddHeightTile(54);
+            Rows[35].Columns[24].AddHeightTile(51);
 
-            Rows[3].Columns[7].AddBaseTile(31);
-            Rows[4].Columns[7].AddBaseTile(26);
-            Rows[5].Columns[7].AddBaseTile(29);
+            Rows[38].Columns[23].AddHeightTile(51);
+            Rows[39].Columns[23].AddHeightTile(50);
+            Rows[38].Columns[24].AddHeightTile(55);
 
-            Rows[4].Columns[6].AddBaseTile(104);
+            Rows[34].Columns[24].AddHeightTile(54);
+
+            Rows[34].Columns[25].AddHeightTile(62);
+            Rows[34].Columns[25].AddHeightTile(61);
+            Rows[34].Columns[25].AddHeightTile(63);
+
 
             // End Create Sample Map Data
         }
 
-		public void Update(GameTime gameTime, Vector2 pPos, GraphicsDevice graphDev)
+        public void Update(GameTime gameTime, Vector2 pPos, GraphicsDevice graphDev)
 		{
 			camMoveVert = true;
 			camMoveHoriz = true;

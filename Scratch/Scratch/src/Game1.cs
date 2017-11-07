04198c8456bf10aeb36c0fd3bd8b77d3bb90c969
vraykilla <vray25.vl@gmail.com>
2017-11-07 13:12:25 -0700

    Start/end screen + music

--- Scratch/Scratch/src/Game1.cs
+++ Scratch/Scratch/src/Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Scratch
 {
@@ -16,6 +17,16 @@ namespace Scratch
 		private ItemsOnScreen items;
 		Vector2 enemyP;
 
+		private Texture2D startButton;
+		private Texture2D exitButton;
+		private Texture2D endButton;
+		private Vector2 startButtonPos;
+		private Vector2 exitButtonPos;
+		private Vector2 endButtonPos; 
+		enum GameState{StartMenu,EndMenu,Playing}
+		GameState gameState; 
+		private Song backGround;
+
 		static int squaresAcross = 17;
 		static int squaresDown = 37;
 		TileMap myMap = new TileMap(squaresDown, squaresAcross);
@@ -29,6 +40,10 @@ namespace Scratch
 		protected override void Initialize()
 		{
 			//TODO: Add your initialization logic here
+			startButtonPos = new Vector2((GraphicsDevice.Viewport.Width / 2)-400, 200);
+			exitButtonPos = new Vector2((GraphicsDevice.Viewport.Width / 2) -50, 200);
+			endButtonPos = new Vector2((GraphicsDevice.Viewport.Width/2), 100);
+			gameState = GameState.StartMenu;
 			base.Initialize();
 		}
 
@@ -36,6 +51,11 @@ namespace Scratch
 		{
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch(GraphicsDevice);
+			startButton = Content.Load<Texture2D>("start");
+			exitButton = Content.Load<Texture2D>("exit");
+			endButton = Content.Load<Texture2D>("gameover2");
+			backGround = Content.Load<Song>("media/background");
+			MediaPlayer.Play(backGround);
 			Texture2D zombieTexture = Content.Load<Texture2D>("zombie_0");
 			Texture2D playerTexture = Content.Load<Texture2D>("player");
 			Texture2D itemTexture = Content.Load<Texture2D>("hammer1");
@@ -54,31 +74,71 @@ namespace Scratch
 
 		protected override void Update(GameTime gameTime)
 		{
-			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-				Exit();
-			// TODO: Add your update logic here
-			if (IsActive)
+			if (gameState == GameState.StartMenu)
 			{
-				//				Texture2D tex = myMap.text;
-				player.Update(gameTime, this.GraphicsDevice, myMap.camMoveVert, myMap.camMoveHoriz);
-				zombie.Update(gameTime, player.pos, myMap.camMoveVert, myMap.camMoveHoriz);
-				zombie1.Update(gameTime, player.pos, myMap.camMoveVert, myMap.camMoveHoriz);
-				Random rnd = new Random();
-				if (rnd.Next(1, 100) % 2 == 1) enemyP = zombie.ePos;
-				else enemyP = zombie1.ePos;
-				items.Update(gameTime, player.angle, myMap.camMoveVert, myMap.camMoveHoriz, player.spd, player.pos, enemyP);
-				base.Update(gameTime);
-				myMap.Update(gameTime, player.pos, this.GraphicsDevice);
+				KeyboardState keys = Keyboard.GetState();
 
-				if (player.BoundingBox.Intersects(zombie.BoundingBox) || player.BoundingBox.Intersects(zombie1.BoundingBox))
+				if (keys.IsKeyDown(Keys.Enter))
 				{
-					player.lives--;
-					if (player.lives == 0)
-						Exit();
-					Random rnd1 = new Random();
-					player.pos.X = rnd1.Next(500);
-					player.pos.Y = rnd1.Next(500);
 
+					gameState = GameState.Playing;
+
+				}
+				if (keys.IsKeyDown(Keys.Back))
+				{
+					Exit();
+				}
+			}
+
+
+			if (gameState == GameState.EndMenu)
+			{
+				KeyboardState keys = Keyboard.GetState();
+
+				if (keys.IsKeyDown(Keys.Enter))
+				{
+					player.lives = 3; 
+					gameState = GameState.Playing;
+
+				}
+				if (keys.IsKeyDown(Keys.Back))
+				{
+					Exit();
+				}
+			}
+					
+
+
+
+			if (gameState == GameState.Playing){
+
+				if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+					Exit();
+				// TODO: Add your update logic here
+				if (IsActive)
+				{
+					//				Texture2D tex = myMap.text;
+					player.Update(gameTime, this.GraphicsDevice, myMap.camMoveVert, myMap.camMoveHoriz);
+					zombie.Update(gameTime, player.pos, myMap.camMoveVert, myMap.camMoveHoriz);
+					zombie1.Update(gameTime, player.pos, myMap.camMoveVert, myMap.camMoveHoriz);
+					Random rnd = new Random();
+					if (rnd.Next(1, 100) % 2 == 1) enemyP = zombie.ePos;
+					else enemyP = zombie1.ePos;
+					items.Update(gameTime, player.angle, myMap.camMoveVert, myMap.camMoveHoriz, player.spd, player.pos, enemyP);
+					base.Update(gameTime);
+					myMap.Update(gameTime, player.pos, this.GraphicsDevice);
+
+					if (player.BoundingBox.Intersects(zombie.BoundingBox) || player.BoundingBox.Intersects(zombie1.BoundingBox))
+					{
+						player.lives--;
+						if (player.lives == 0)
+							gameState = GameState.EndMenu; 
+							//Exit();
+						Random rnd1 = new Random();
+						player.pos.X = rnd1.Next(500);
+						player.pos.Y = rnd1.Next(500);
+
+					}
 				}
 			}
 		}
@@ -86,12 +146,32 @@ namespace Scratch
 		protected override void Draw(GameTime gameTime)
 		{
 			spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend);
-			myMap.Draw(graphics, spriteBatch);
-			items.Draw(spriteBatch);
-			zombie.Draw(spriteBatch, zombie.ePos);
-			zombie1.Draw(spriteBatch, zombie1.ePos);
-			player.Draw(spriteBatch, player.pos);
-			base.Draw(gameTime);
+			if (gameState == GameState.StartMenu)
+			{
+				graphics.GraphicsDevice.Clear(Color.Crimson);
+				spriteBatch.Draw(startButton, startButtonPos, Color.White);
+				spriteBatch.Draw(exitButton, exitButtonPos, Color.White);
+			}
+
+			else if (gameState == GameState.EndMenu)
+			{
+				graphics.GraphicsDevice.Clear(Color.Crimson);
+				spriteBatch.Draw(endButton, endButtonPos, Color.White);
+			}
+
+
+
+			else if (gameState == GameState.Playing)
+			{
+				
+				myMap.Draw(graphics, spriteBatch);
+				items.Draw(spriteBatch);
+				zombie.Draw(spriteBatch, zombie.ePos);
+				zombie1.Draw(spriteBatch, zombie1.ePos);
+				player.Draw(spriteBatch, player.pos);
+				base.Draw(gameTime);
+
+			}
 			spriteBatch.End();
 		}
 	}

9804b8d1ff3dc9ca02ed8117f9d9046e28d843a1
Mark Kircher <zatuke@hotmail.com>
2017-11-24 19:13:11 -0700

    forgot the bullet icon and other content changes so this is the final part of the prior commit made by me

---
+++ Scratch/Scratch/src/Sprites/Bullet.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+
+namespace Scratch.src{
+
+    public class Bullet{
+
+        public Rectangle bounding;
+        public Texture2D Texture;
+        public Vector2 orgin;
+        public Vector2 position;
+        public bool isVisibile;
+        public float speed;
+        public float? angle;
+
+        //contructorrr
+        public Bullet(Texture2D newtexture){
+            speed = 10;
+            Texture = newtexture;
+            isVisibile = false;
+        }
+
+        public void Draw(SpriteBatch spriteBatch){
+
+            spriteBatch.Draw(Texture, position, Color.White);
+        }
+
+        public Texture2D Textureget(){
+
+            return Texture;
+        }
+    }
+}

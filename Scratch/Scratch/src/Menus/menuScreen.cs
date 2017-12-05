ebde6717e6188eb2744c64ec0a3ba2471e8f2934
vraykilla <vray25.vl@gmail.com>
2017-12-05 00:27:46 -0700

    quick fix to start screen

--- Scratch/Scratch/src/Menus/menuScreen.cs
+++ Scratch/Scratch/src/Menus/menuScreen.cs
@@ -7,45 +7,54 @@ using Microsoft.Xna.Framework.Media;
 
 namespace Scratch {
 	public class menuScreen {
-
+		//buttons made of Texture2D
 		private Texture2D startButton;
-		private Texture2D exitButton;
 		private Texture2D endButton;
 		private Texture2D resumeButton;
+
+		//positions of buttons on screen 
 		private Vector2 startButtonPos;
-		private Vector2 exitButtonPos;
 		private Vector2 endButtonPos;
 		private Vector2 resumeButtonPos;
+		private Vector2 fontPos;
+
+		//font and media used 
 		private SpriteFont font;
-		private Vector2 fontPos; 
 		private Song backGround;
 
+		//create different gamestates 
 		public enum GameState { StartMenu, EndMenu, Playing, Paused,}
 		public GameState gameState;
 
 		public menuScreen() {
 		}
 
+		/**
+		 * @param- graphics used to get the viewport of the window 
+		 * initialize the positions and gamestate 
+		 **/
 		public void Initialize(GraphicsDeviceManager graphics){
 			startButtonPos = new Vector2((graphics.GraphicsDevice.Viewport.Width / 2) - 400, 200);
-			exitButtonPos = new Vector2((graphics.GraphicsDevice.Viewport.Width / 2) - 50, 200);
 			endButtonPos = new Vector2((graphics.GraphicsDevice.Viewport.Width / 2) - 250, 200);
 			resumeButtonPos = new Vector2((graphics.GraphicsDevice.Viewport.Width / 2), 100);
 			fontPos = new Vector2 (10, 10); 
 			gameState = GameState.StartMenu;
 		}
 
+		/**
+		 * @param- ContentManager where all our content is stored
+		 * will load the pictures I need, font, and song
+		 **/
 		public void LoadContent(ContentManager Content){
 			startButton = Content.Load<Texture2D>("menus/start");
-			exitButton = Content.Load<Texture2D>("menus/exit");
 			endButton = Content.Load<Texture2D>("menus/generatedtext");
 			resumeButton = Content.Load<Texture2D>("menus/resume");
 			font = Content.Load<SpriteFont> ("sprites/font");
 			backGround = Content.Load<Song>("media/background");
-			MediaPlayer.Play(backGround);
+			MediaPlayer.Play(backGround);//starts the song 
 
 		}
-
+		//determine the games states from user input 
 		public void Update(){
 			KeyboardState keys = Keyboard.GetState();
 
@@ -70,20 +79,23 @@ namespace Scratch {
 
 			}
 		}
-
+		/**
+		 * @param- graphics, spritebatch
+		 * graphics is used to clear the background of the window and set to black
+		 * spritebatch is used to draw the buttons and string 
+		 */
 		public void StartDraw(GraphicsDeviceManager graphics, SpriteBatch spriteBatch) {
-			graphics.GraphicsDevice.Clear(Color.Crimson);
+			graphics.GraphicsDevice.Clear(Color.Black);
 			spriteBatch.Draw(startButton, startButtonPos, Color.White);
-			spriteBatch.Draw(exitButton, exitButtonPos, Color.White);
 			string text = "Push enter to play\n(a-s-w-d) player movement\nc to combine items in inventory \nf to use first aid\nr to drop mine\np to pause\nspace to fire bullet\n";
-			spriteBatch.DrawString (font, text, fontPos, Color.Black);
+			spriteBatch.DrawString (font, text, fontPos, Color.Red);
 		}
 
 		public void ResumeDraw (GraphicsDeviceManager graphics, SpriteBatch spriteBatch) {
-			graphics.GraphicsDevice.Clear (Color.Crimson);
+			graphics.GraphicsDevice.Clear (Color.Black);
 			spriteBatch.Draw (resumeButton, resumeButtonPos, Color.White);
 			string text = "Push r to resume\n(a-s-w-d) player movement\nc to combine items in inventory \nf to use first aid\nr to drop mine\np to pause\nspace to fire bullet\n";
-			spriteBatch.DrawString (font, text, fontPos, Color.Black);
+			spriteBatch.DrawString (font, text, fontPos, Color.Red);
 		}
 
 		public void EndDraw(GraphicsDeviceManager graphics, SpriteBatch spriteBatch) {
